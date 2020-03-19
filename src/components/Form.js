4381dec14f17f1c5bb82f5798b4823938d2ea73d
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-03-19 18:46:27 -0500

    Edited Members.js. Set up Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,46 @@
 import React, { useState } from "react";
 
+const Form = props => {
+    const [member, setMember] = useState([
+        {
+            name: "",
+            email: "",
+            role: ""
+        }
+    ]);
+
+    const handleChanges = event => {
+    setMember({...member, [even.target.name]: event.target.value });
+    };
+
+    const submitHandler = event => {
+    event.preventDefault();
+    const newMember = {
+        ...member, 
+        id: Date.now()
+    };
+
+    return(
+        <div>
+            <form onSubmit= {submitHandler}>
+                <label htmlFor="name">Name: </label>
+                <input
+                    onChange= {handleChanges}
+                    id="name"
+                    name="name"
+                    value={member.name}
+                />
+                <label htmlFor="role">Role: </label>
+                <input
+                    onChange= {handleChanges}
+                    id="role"
+                    value={member.role}
+                />
+                <button type="submit"> Add New Member</button>
+                </form>
+        </div>
+    ); 
+};
+}
 
 export default Form;
